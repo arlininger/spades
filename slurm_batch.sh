429bb6ffd0ac9b7aa8cf8fc93a35952f6f061bd4
Adam Lininger <arlininger@gmail.com>
2021-11-06 21:34:29 -0500

    combine submission scripts

--- slurm_batch.sh
+++ slurm_batch.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 #SBATCH --job-name=spades             # Job name
-#SBATCH --ntasks=24                   # Run on a single CPU
 #SBATCH --output=spades.log           # Standard output and error log
-pwd; hostname; date
+#SBATCH --ntasks=24
 
 cd $SLURM_SUBMIT_DIR
 
-srun /clusterfs/usr/bin/python3 run_spades.py
+# /usr/bin/mpiexec --mca plm_base_verbose 10 -n 24 /clusterfs/usr/bin/python3 run_spades.py
+/usr/bin/mpiexec -n 24 /clusterfs/usr/bin/python3 run_spades.py
+
